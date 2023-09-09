7d4dc034d75a324a7898b295a6b098229caaee60
JOAQUIN RODAL <informatico.joaquin@gmail.com>
2023-09-09 04:52:02 +0100

    Create alpine_ajax.js

---
+++ alpine_ajax.js
@@ -0,0 +1,16 @@
+
+<script defer src="/static/js/alpine_ajax.min.js"></script>
+
+<a href="/comentario" x-target="comentario comentario2" >mensaje</a>
+x-target --> destino de lo que devuelve el servidor
+
+<div id="mensaje"></div>
+<div id="mensaje2"></div>
+
+lo que devuelve el servidor tiene que ser como esto :
+<div id="mensaje">
+    JOAQUIN
+</div>
+<div id="mensaje2">
+    ADRIANA
+</div>
